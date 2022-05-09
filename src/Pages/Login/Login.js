5ef14e06fd5a6e9c244a6c0fa2fee6c7818aaeb2
Kibria934 <kibriahossain725@gmai.com>
2022-05-09 23:51:08 +0600

    all set

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css'
+
+
+const Login = () => {
+    return (
+        <div>
+            <h3>This is login </h3>
+        </div>
+    );
+};
+
+export default Login;
